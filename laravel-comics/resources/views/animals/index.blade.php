b2578d70af75192de6008738b58d6ceffdf4fb9a
CodeKiro <kirofiniello@outlook.it>
2024-07-29 18:24:23 +0200

    ho utilizzato il layout correttamente

--- laravel-comics/resources/views/animals/index.blade.php
+++ laravel-comics/resources/views/animals/index.blade.php
@@ -1,42 +1,31 @@
 @extends('layouts.app')
 
-@section('page-title', 'Animals')
+@section('page-title', 'index-animal')
 
+@section('style')
+<style>
+    .animal-img {
+        height: 200px;
+        object-fit: contain;
+    }
+</style>
+@endsection
 
 @section('main-content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Animals</title>
-    <style>
-        .animal-img {
-            height: 200px;
-            object-fit: cover;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">Animals</h1>
-        <div class="row">
-            @foreach ($animals as $animal)
-            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-                <div class="card">
-                    <img src="{{ $animal['image_url'] }}" class="card-img-top animal-img" alt="{{ $animal['title'] }}">
-                    <div class="card-body">
-                        <h3 class="card-title">{{ $animal['title'] }}</h3>
-                        <h6 class="card-subtitle text-muted">{{ $animal['category'] }}</h6>
-                    </div>
+<div class="container">
+    <h1 class="text-center my-5">Animals</h1>
+    <div class="row">
+        @foreach ($animals as $animal)
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card">
+                <img src="{{ $animal['image_url'] }}" class="card-img-top animal-img" alt="{{ $animal['title'] }}">
+                <div class="card-body">
+                    <h3 class="card-title">{{ $animal['title'] }}</h3>
+                    <h6 class="card-subtitle text-muted">{{ $animal['category'] }}</h6>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+</div>
+@endsection
--- laravel-comics/resources/views/layouts/app.blade.php
+++ laravel-comics/resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @yield('style')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -16,8 +17,10 @@
         @yield('main-content')
     </main>
 
-    @include('pages.partials.footer')
-
     @yield('custom-scripts')
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
